c2be58380f9f23f354bcbae8e3949fb414e347f2
Shubham Saurav <shubhamsinha186@gmail.com>
2020-10-10 22:51:30 +0530

    Answer files added

---
+++ controllers/answers.js
@@ -0,0 +1,4 @@
+exports.getAnswer = async (req, res, next) => {};
+exports.createAnswer = async (req, res, next) => {};
+exports.updateAnswer = async (req, res, next) => {};
+exports.deleteAnswer = async (req, res, next) => {};
