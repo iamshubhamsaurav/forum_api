fa2f9d7e2be254d9acb45a8953c184eec5be99a3
Shubham Saurav <shubhamsinha186@gmail.com>
2020-10-14 23:30:17 +0530

    Added getAnswer

--- controllers/answers.js
+++ controllers/answers.js
@@ -1,5 +1,45 @@
-exports.getAnswers = async (req, res, next) => {};
-exports.getAnswer = async (req, res, next) => {};
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/AppError');
+const APIFeatures = require('../utils/apiFeatures');
+const Question = require('../models/Question');
+const Answer = require('../models/Answer');
+
+// @route       : GET /api/v1/blogs/:id
+// @desc        : Get a blog
+// @access      : Public
+exports.getAnswers = catchAsync (async (req, res, next) => {
+    
+});
+
+// @route       : GET /api/v1/answers/:id
+// @desc        : Get a blog
+// @access      : Public
+exports.getAnswer = async (req, res, next) => {
+    const answer = await Answer.findById(req.params.id);
+    if (!answer) {
+        return next(new AppError(`No Answer found with id of ${req.params.id}`, 404));
+    }
+    const question = await Question.findById(answer.questionId);
+    res.status(200).json({
+        success: true,
+        data: {
+            question: question,
+            answer: answer
+        }
+    });
+};
+
+// @route       : GET /api/v1/blogs/:id
+// @desc        : Get a blog
+// @access      : Public
 exports.createAnswer = async (req, res, next) => {};
+
+// @route       : GET /api/v1/blogs/:id
+// @desc        : Get a blog
+// @access      : Public
 exports.updateAnswer = async (req, res, next) => {};
+
+// @route       : GET /api/v1/blogs/:id
+// @desc        : Get a blog
+// @access      : Public
 exports.deleteAnswer = async (req, res, next) => {};
