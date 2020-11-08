3c355f87de2ddf9ebd32d999f932c0800398e348
Shubham Saurav <shubhamsinha186@gmail.com>
2020-11-08 15:12:35 +0530

    Tested All Answers Routes

--- controllers/answers.js
+++ controllers/answers.js
@@ -3,10 +3,13 @@ const AppError = require('../utils/apiError');
 const Question = require('../models/Question');
 const Answer = require('../models/Answer');
 
-// @route       : GET /api/v1/question/:questionId/answers
+// @route       : GET /api/v1/questions/:questionId/answers
 // @desc        : Get Answers using questionId
 // @access      : Public
 exports.getAnswersByQuestion = catchAsync (async (req, res, next) => {
+    if (!req.params.questionId) {
+        return next(new AppError(`Please provide a questionId`, 400));
+    }
     const answers = await Answer.find({questionId: req.params.questionId});
     res.status(200).json({
         success: true,
@@ -23,26 +26,32 @@ exports.getAnswer = async (req, res, next) => {
     if (!answer) {
         return next(new AppError(`No Answer found with id of ${req.params.id}`, 404));
     }
-    const question = await Question.findById(answer.questionId);
     res.status(200).json({
         success: true,
-        data: {
-            question: question,
-            answer: answer
-        }
-    });
-};
-
-// @route       : POST /api/v1/answers
-// @desc        : Create An Answer
-// @access      : Private
-exports.createAnswer = async (req, res, next) => {
-    const answer = await Answer.create(req.body);
-    res.status(200).json({
-        success: answer,
         data: answer
     });
 };
+
+// @route       : POST /api/v1/questions/:questionId/answers
+// @desc        : Create An Answer
+// @access      : Private
+exports.createAnswer = async (req, res, next) => {
+    if (!req.params.questionId) {
+        return next(new AppError(`Please provide a questionId`, 400));
+    }
+    const question = await Question.findById(req.params.questionId);
+    if (!question) {
+        return next(new AppError(`No Question found with id of ${req.params.questionId}`, 404));
+    }
+    req.body.questionId = req.params.questionId;
+    const answer = await Answer.create(req.body);
+    res.status(200).json({
+        success: true,
+        data: answer
+    });
+};
+
+// TODO: Check the routes and its structures. Also think about virtuals
 
 // @route       : PUT /api/v1/answers/:id
 // @desc        : Update an answer
--- routes/questions.js
+++ routes/questions.js
@@ -5,7 +5,7 @@ const answerRoute = require('./answers');
 
 const router = express.Router();
 
-router.route('/:questionId/answers', answerRoute);
+router.use('/:questionId/answers', answerRoute);
 
 router
   .route('/')
